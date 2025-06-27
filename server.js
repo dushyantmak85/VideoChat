bfd32d80d26a0b7a44df1cb4f5dfbaeb22d94da3
Dushyant Makwana <dushyantmakwana2004@gmail.com>
2025-06-27 23:31:03 +0530

    added the ejs file

---
+++ server.js
@@ -0,0 +1,8 @@
+const express= require('express');
+const app = express();
+const server=require('http').Server(app);
+const io=require('socket.io')(server);
+
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
